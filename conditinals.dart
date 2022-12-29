9ab51923bc9664754e29182b8c40abb367a1ca62
ashishpandey2600 <ashishpandey@Ashishs-MacBook-Pro.local>
2022-12-29 08:29:37 +0530

    new files

---
+++ conditinals.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  //arithematic + - / * %  ~/
+  //~/ gives only integer value
+  //Increment and Decrement ++ --
+  //ASssignment =
+  //Logical && || !
+  //Conditional ?: ??
+  int a = 20;
+  print(a > 20 ? "a is greater then 20 " : "a is less then 20");
+  
+}
